bf19a0c2dadc017a7925a842425678ea23a7c91d
runesam <same7.hamada@gmail.com>
2018-11-17 21:55:54 +0100

    [#feat] middleware and logger

--- serverApp/index.js
+++ serverApp/index.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import hbs from 'hbs';
+import fs from 'fs';
 
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 hbs.registerHelper('getCurrentYear', () => new Date().getUTCFullYear());
@@ -10,6 +11,22 @@ const app = express();
 const port = 3000;
 
 // USE
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const log = `${now}: ${req.method} ~ ${req.url}`;
+    fs.appendFile(
+        path.join(__dirname, 'server.log'),
+        `${log} \n`,
+        err => console.log(err, 'can\'t save file'),
+    );
+    console.log(log);
+    next();
+});
+app.use((req, res) => {
+    res.render('maintenance.hbs', {
+        pageTitle: 'sorry :( we are',
+    });
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 // SET
