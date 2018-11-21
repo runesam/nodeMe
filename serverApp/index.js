9dbbedcfa21f27a1d000b0173b94bb9ce1239126
runesam <same7.hamada@gmail.com>
2018-11-21 07:54:00 +0100

    set for heoku

--- serverApp/index.js
+++ serverApp/index.js
@@ -9,7 +9,7 @@ hbs.registerHelper('getCurrentYear', () => new Date().getUTCFullYear());
 hbs.registerHelper('getWelcomeMessage', (by, to) => `welcome by ${by} to my first ${to}`);
 
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 // USE
 app.use((req, res, next) => {
