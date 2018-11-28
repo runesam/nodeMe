ef130d90604b0a53f1c33a85d9f1b586f3faf130
runesam <same7.hamada@gmail.com>
2018-11-28 08:27:35 +0100

    create message event listeners

--- chatApp/index.js
+++ chatApp/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import http from 'http';
 import socketIO from 'socket.io';
 
-export const app = express();
+const app = express();
 const publicPath = path.join(__dirname, '/public');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
@@ -17,11 +17,20 @@ const onConnection = (res) => {
 	console.log('connected to client', res.id);
 };
 
+const onCreateMessage = (data, socket) => {
+	const { text } = data;
+	const createdAt = new Date();
+	const from = socket.id;
+	console.log('server got a message', data);
+	socket.emit('newMessage', { text, createdAt, from });
+};
+
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
 	onConnection(socket);
 	socket.on('disconnect', onDisconnect);
+	socket.on('createMessage', data => onCreateMessage(data, socket));
 });
 
 // app listening
